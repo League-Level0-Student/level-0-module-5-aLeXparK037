b5c539fc3626c392af3ec675027f2ea3da3cb681
aLeXparK037 <65091873+aLeXparK037@users.noreply.github.com>
2020-07-15 13:28:22 -0700

    34

---
+++ src/_02_nested_loops/_3_for_loop_gauntlet/fun.java
@@ -0,0 +1,11 @@
+package _02_nested_loops._3_for_loop_gauntlet;
+
+import java.util.Random;
+
+public class fun {
+public static void main(String[] args) {
+	Random ran = new Random();
+	int number = ran.nextInt(100);
+	System.out.println(number);
+}
+}
